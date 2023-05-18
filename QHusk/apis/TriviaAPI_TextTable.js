b94a9fc5dcf55ad3e567311c7d24abddb8c69ddc
Deltainu <109909833+Deltainu@users.noreply.github.com>
2023-05-18 19:54:59 +0200

    correct typo

--- QHusk/apis/TriviaAPI_TextTable.js
+++ QHusk/apis/TriviaAPI_TextTable.js
@@ -1,7 +1,7 @@
 const conversation = [
     "\n__________________________________________________\n",
     "\nWillkommen im TriviaApi Grabber.\n",
-    "\nEs werden pro Anfrage 20 Fragen generiert. Wie oft soll dieser Vorgang wiederholt werden?\n",
+    "\nEs werden pro Anfrage 10 Fragen generiert. Wie oft soll dieser Vorgang wiederholt werden?\n",
     "\nVielen Dank für die Informationen, Ihre Datei befindet sich nun in der Grabberschmiede und wird hergestellt\n",
     "\nIhre Datein wurde erfolgreich erstellt, Ich hoffe alles ist zu Ihrer zufriedenheit und Ich wünsche Ihnen einen schönen Tag oder aber bis später.\n"
 ]
